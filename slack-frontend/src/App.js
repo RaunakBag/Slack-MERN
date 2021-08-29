05f5139a2118bbe66de9c313ad9a85ae97095f6f
RaunakBag <raunakbag97@gmail.com>
2021-08-29 21:52:39 +0530

    Working

--- slack-frontend/src/App.js
+++ slack-frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import { useStateValue } from "./StateProvider"
+import Login from "./Login"
+import Header from "./Header"
+import Sidebar from "./Sidebar"
+import Chat from "./Chat"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [{ user }] = useStateValue()
+
+	return (
+		<div className="app">
+			<Router>
+				{!user ? (
+					<Login />
+				) : (
+					<>
+						<Header />
+						<div className="app__body">
+							<Sidebar />
+							<Switch>
+								<Route path="/room/:roomId">
+									<Chat />
+								</Route>
+								<Route path="/">
+									<div className="app__main">
+									</div>
+								</Route>
+							</Switch>
+						</div>
+					</>
+				)}
+			</Router>
+		</div>
+	)
 }
 
-export default App;
+export default App
